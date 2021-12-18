8b5cb24c5382f5b2d354f27394e2cb1e05501a03
zalfrie januardi <zalfrie@protonmail.com>
2021-12-18 16:36:51 +0700

    testing register endpoint

--- tests/Feature/RegisterTest.php
+++ tests/Feature/RegisterTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use App\Models\User;
 
-class RegistrationTest extends TestCase
+class RegisterTest extends TestCase
 {
 
     use DatabaseMigrations;
