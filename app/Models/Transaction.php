da3382d2f4bb35f7792af980db88e3735811e12f
zalfrie januardi <zalfrie@protonmail.com>
2021-12-18 21:33:33 +0700

    Delete view blade

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -10,14 +10,4 @@ class Transaction extends Model
     use HasFactory;
 
     protected $guarded = [];
-
-    public function merchant()
-    {
-        return $this->belongsTo(Merchant::class, 'merchant_id', 'id');
-    }
-
-    public function outlet()
-    {
-        return $this->belongsTo(Outlet::class, 'outlet_id', 'id');
-    }
 }
